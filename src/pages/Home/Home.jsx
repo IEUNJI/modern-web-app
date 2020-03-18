8d270a4b66d871a552d50c1e07a04a7c22d85398
linzijun <linzjk@yonyou.com>
2020-03-18 16:29:46 +0800

    完善首页导航

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -5,12 +5,81 @@ import './Home.less';
 class Home extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      navList: [
+        {
+          text: '相机',
+          textColor: '#fff',
+          bgColor: 'lightcoral',
+          path: '/camera'
+        },
+        {
+          text: '工具',
+          textColor: '#fff',
+          bgColor: 'lightseagreen'
+        },
+        {
+          text: '工具',
+          textColor: '#fff',
+          bgColor: 'lightsalmon'
+        },
+        {
+          text: '工具',
+          textColor: '#fff',
+          bgColor: 'lightskyblue'
+        },
+        {
+          text: '工具',
+          textColor: '#fff',
+          bgColor: 'lightpink'
+        },
+        {
+          text: '工具',
+          textColor: '#fff',
+          bgColor: 'lightgreen'
+        },
+        {
+          text: '工具',
+          textColor: '#888',
+          bgColor: 'lightcyan'
+        },
+        {
+          text: '工具',
+          textColor: '#fff',
+          bgColor: 'lightsteelblue'
+        },
+        {
+          text: '工具',
+          textColor: '#888',
+          bgColor: 'lightgoldenrodyellow'
+        }
+      ]
+    };
+  }
+
+  navigator = path => {
+    path && this.props.history.push(path);
   }
 
   render() {
+    const { navList } = this.state;
+
     return (
       <section id="home-page">
-        Home
+        <section className="nav-container">
+          {
+            navList.map((nav, index) => {
+              const { text, textColor, bgColor, path } = nav;
+              return (
+                <nav key={`nav-${index}`} style={{ background: bgColor }} onClick={this.navigator.bind(this, path)}>
+                  <span style={{ color: textColor }}>
+                    {text}
+                  </span>
+                </nav>
+              );
+            })
+          }
+        </section>
       </section>
     );
   }
