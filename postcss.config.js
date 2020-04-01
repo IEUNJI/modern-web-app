96960e9afd9dbae9e40b69656ed44796bf6cc367
linzijun <linzjk@yonyou.com>
2020-04-01 11:04:22 +0800

    增加css适配

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,17 @@
 module.exports = {
-  plugins: [
-    require('autoprefixer')
-  ]
+  plugins: {
+    'autoprefixer': {},
+    'postcss-px-to-viewport': {
+      unitToConvert: 'px',
+      viewportWidth: 375,
+      unitPrecision: 5,
+      viewportUnit: 'vw',
+      fontViewportUnit: 'vw',
+      minPixelValue: 1,
+      exclude: /node_modules/,
+      landscape: true,
+      landscapeUnit: 'vw',
+      landscapeWidth: 667
+    }
+  }
 };
