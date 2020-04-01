aa0b6920527e54aa4607fd8a45650d1a44c5df83
linzijun <linzjk@yonyou.com>
2020-04-01 17:18:48 +0800

    删除冗余样式

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import 'normalize.css';
-import 'antd-mobile/dist/antd-mobile.less'
+import 'antd-mobile/dist/antd-mobile.less';
 
 import App from 'pages/App/App';
 
