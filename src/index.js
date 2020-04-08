adf5fa9d6c422635ccd787fd3dda564730eb1b74
linzijun <linzjk@yonyou.com>
2020-04-08 13:56:44 +0800

    修正vh误差

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import ReactDOM from 'react-dom';
 import 'normalize.css';
 import 'antd-mobile/dist/antd-mobile.less';
 
+import vhCheck from 'vh-check';
+vhCheck();
+
 import App from 'pages/App/App';
 
 ReactDOM.render(<App />, document.querySelector('#root'));
