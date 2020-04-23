66a42731ad26cf819adf128c949247065c7f78d6
linzijun <linzjk@yonyou.com>
2020-04-23 17:08:59 +0800

    完善详情页

--- src/pages/Detail/Detail.jsx
+++ src/pages/Detail/Detail.jsx
@@ -1,17 +1,40 @@
 import React from 'react';
+
 import './Detail.less';
 
 class Detail extends React.Component {
   constructor(props) {
     super(props);
+    this.detailPageInstance = React.createRef();
+  }
+
+  componentDidMount() {
+    this.fetchArticleData();
+  }
+
+  fetchArticleData = () => {
+    const { id } = this.props.match.params;
+    fetch(`https://zhihu-daily.leanapp.cn/api/v1/contents/${id}`)
+      .then(res => res.json())
+      .then(res => {
+        const articleData = res.CONTENTS;
+        const articleHtml = articleData.body;
+        const articleCss = articleData.css[0];
+        const link = document.createElement('link');
+        link.id = 'mwa-css-detail';
+        link.rel = 'stylesheet';
+        link.href = articleCss;
+        if (!document.querySelector('#mwa-css-detail')) {
+          document.head.appendChild(link);
+        }
+        this.detailPageInstance.current.innerHTML = articleHtml;
+      });
   }
 
   render() {
     return (
-      <div id="detail-page">
-        {
-          this.props.match.params.id
-        }
+      <div id="detail-page" ref={this.detailPageInstance}>
+        
       </div>
     );
   }
