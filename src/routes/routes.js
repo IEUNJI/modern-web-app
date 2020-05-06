5e663499d2726e2265edc54cf0ad9502ffe3aa22
linzijun <linzjk@yonyou.com>
2020-05-06 12:34:27 +0800

    删除详情页

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,5 +1,4 @@
 import Home from 'pages/Home/Home';
-import Detail from 'pages/Detail/Detail';
 import Editor from 'pages/Editor/Editor';
 
 export default [
@@ -8,12 +7,6 @@ export default [
     path: '/',
     component: Home,
     title: '首页'
-  },
-  {
-    exact: true,
-    path: '/detail/:id',
-    component: Detail,
-    title: '详情页'
   },
   {
     exact: true,
