4a7b4825542f1ce7c4e191f37109b7c6c1e6ab5e
linzijun <linzjk@yonyou.com>
2020-05-07 12:48:43 +0800

    首页增加电池状态信息

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -5,6 +5,36 @@ import './Home.less';
 class Home extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      charging: false,
+      level: 1
+    };
+  }
+
+  componentDidMount() {
+    this.batteryManager();
+  }
+
+  batteryManager = () => {
+    navigator.getBattery().then(battery => {
+      const { charging, level } = battery;
+      this.setState({
+        charging,
+        level
+      });
+      battery.addEventListener('chargingchange', () => {
+        const { charging } = battery;
+        this.setState({
+          charging
+        });
+      });
+      battery.addEventListener('levelchange', () => {
+        const { level } = battery;
+        this.setState({
+          level
+        });
+      });
+    });
   }
 
   openConsole = () => {
@@ -19,8 +49,12 @@ class Home extends React.Component {
   }
 
   render() {
+    const { charging, level } = this.state;
     return (
       <div id="home-page">
+        <span>{charging ? '充电中：' : '未充电：'}</span>
+        <span>{level * 100 + '%'}</span>
+        <hr />
         <button onClick={this.openConsole}>Open Console</button>
         <hr />
         <button onClick={() => this.props.history.push('/editor')}>图片编辑页</button>
